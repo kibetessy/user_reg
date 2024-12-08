8aba746dcdd88df8dc09fe14dc31ac93de0a31bf
calebkech <calebkemboi61@gmail.com>
2024-12-08 15:36:49 +0300

    Created util file with send_email function

--- core/forms.py
+++ core/forms.py
@@ -5,8 +5,6 @@ from .models import CustomUser
 
 
 class RegistrationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput, label="Password")
-    confirm_password = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
 
     class Meta:
         model = CustomUser
@@ -14,10 +12,4 @@ class RegistrationForm(forms.ModelForm):
 
     def clean(self):
         cleaned_data = super().clean()
-        password = cleaned_data.get("password")
-        confirm_password = cleaned_data.get("confirm_password")
-
-        if password != confirm_password:
-            raise ValidationError("Passwords do not match.")
-
         return cleaned_data
